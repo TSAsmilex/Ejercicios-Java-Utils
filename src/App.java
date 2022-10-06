d33d9965be707bc47543e634d651e030f8089492
Andrés Millán <andres.millan-munoz@t-systems.com>
2022-10-06 13:00:10 +0200

    WIP ejercicio iteradores

--- src/App.java
+++ src/App.java
@@ -1,10 +1,18 @@
 import java.util.ArrayList;
+import java.util.Arrays;
+
+import javax.swing.text.html.HTMLDocument.Iterator;
 
 public class App {
     public static void main(String[] args) throws Exception {
         var array = ejercicio1(10);
 
-        array.forEach(n -> System.out.println(n + ", "));
+        array.forEach(n -> System.out.print(n + ", "));
+        System.out.println("\n");
+
+        String Apetecan = "Apetecan";
+        System.out.println("Apetecan -> " + iteradores(Apetecan, "a"));
+
     }
 
     public static ArrayList<Integer> ejercicio1(int n) {
@@ -15,5 +23,23 @@ public class App {
         }
 
         return array;
+    }
+
+    public static String iteradores(String cadena, String letra) {
+        var cadena_arraylist = new ArrayList<String>(
+            Arrays.asList(cadena.split(""))
+        );
+
+        var it = cadena_arraylist.iterator();
+
+        while (it.hasNext()) {
+            String actual = it.next();
+
+            if (actual.compareTo(letra) == 0) {
+                it.remove();
+            }
+        }
+
+        return cadena;
     }
 }
